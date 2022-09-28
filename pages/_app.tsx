458b4372894482aa7949c065af0db5e0f6d88f92
yash91989201 <yashraj6370@gmail.com>
2022-09-28 17:42:25 +0530

    add config,packages,icons,absolute import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,26 @@
 import type { AppProps } from 'next/app'
 import { NhostNextProvider, NhostClient } from '@nhost/nextjs';
 import { NhostApolloProvider } from '@nhost/react-apollo'
-import '../styles/globals.css'
 import { NhostSession } from '@nhost/core';
-import { NhostReactProviderProps } from '@nhost/react';
+import '../styles/globals.css'
+// custom components
+import Layout from '../components/shared/Layout';
 
 const nhost = new NhostClient({
   subdomain: process.env.NEXT_PUBLIC_NHOST_SUBDOMAIN || '',
   region: process.env.NEXT_PUBLIC_NHOST_REGION || ''
 });
 
+interface MyAppTypes {
+  nhostSession: NhostSession
+}
 
-
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <NhostNextProvider nhost={nhost} >
+export default function MyApp({ Component, pageProps }: AppProps<MyAppTypes>) {
+  return <NhostNextProvider nhost={nhost} initial={pageProps.nhostSession} >
     <NhostApolloProvider nhost={nhost}>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </NhostApolloProvider>
   </NhostNextProvider>
 }
