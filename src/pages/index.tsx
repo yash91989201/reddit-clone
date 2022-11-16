fc94e2d0c78930da7368278b90598bf55a037eba
yash91989201 <yashraj6370@gmail.com>
2022-11-16 08:15:06 +0530

    style: update postbox positioning

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,23 @@
-import Head from 'next/head'
+import Head from "next/head";
 // custom components
-import PostBox from 'components/home/PostBox'
-import Feed from "components/home/Feed"
+import PostBox from "components/home/PostBox";
+import Feed from "components/home/Feed";
 
 export default function Home(): JSX.Element {
-
-  return <div className="min-h-screen">
-    <Head>
-      <title>Home | Reddit 2.0</title>
-      <meta name="description" content="A NextJs Reddit clone made using nhost " />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <div className="mx-auto max-w-5xl">
-      <PostBox styling={{ top: "top-20" }} />
-      <Feed styling={{ marginTop: "mt-12", marginBottom: "mb-12" }} />
+  return (
+    <div className="min-h-screen">
+      <Head>
+        <title>Home | Reddit 2.0</title>
+        <meta
+          name="description"
+          content="A NextJs Reddit clone made using nhost "
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="max-w-5xl mx-auto">
+        <PostBox styling={{ top: "top-16" }} />
+        <Feed styling={{ marginTop: "mt-12", marginBottom: "mb-12" }} />
+      </div>
     </div>
-  </div>
-
+  );
 }
--- src/pages/subreddit/[topic].tsx
+++ src/pages/subreddit/[topic].tsx
@@ -1,31 +1,36 @@
-import { useRouter } from "next/router"
+import { useRouter } from "next/router";
 // CUSTOM COMPONENTS
-import PostBox from 'components/home/PostBox'
-import Feed from 'components/home/Feed'
-import Avatar from 'components/shared/Avatar'
+import PostBox from "components/home/PostBox";
+import Feed from "components/home/Feed";
+import Avatar from "components/shared/Avatar";
 
 export default function Subreddit(): JSX.Element {
+  const { topic } = useRouter().query;
 
-    const { topic } = useRouter().query
-
-    return <div className="-mt-12">
-        <div className="flex flex-col-reverse my-12 h-64 bg-reddit-col">
-            <div className="bg-white">
-                <div className="flex items-center py-6 mx-auto space-x-4 max-w-5xl">
-                    {/* avatar image */}
-                    <Avatar seed={topic as string} />
-                    {/* subreddit title */}
-                    <div>
-                        <h1 className="text-3xl font-semibold">Welcome to the r/{topic} subreddit</h1>
-                        <p className="text-sm text-gray-400">r/{topic}</p>
-                    </div>
-                </div>
+  return (
+    <div className="-mt-12">
+      <div className="flex flex-col-reverse h-64 my-12 bg-reddit-col">
+        <div className="bg-white">
+          <div className="flex items-center max-w-5xl py-6 mx-auto space-x-4">
+            {/* avatar image */}
+            <Avatar seed={topic as string} />
+            {/* subreddit title */}
+            <div>
+              <h1 className="text-3xl font-semibold">
+                Welcome to the r/{topic} subreddit
+              </h1>
+              <p className="text-sm text-gray-400">r/{topic}</p>
             </div>
+          </div>
         </div>
-        <div className="mx-auto max-w-5xl">
-            <PostBox subreddit={topic as string} styling={{ top: "top-8" }} />
-            <Feed subreddit={topic as string} styling={{ marginTop: "mt-12", marginBottom: "mb-16" }} />
-        </div>
+      </div>
+      <div className="max-w-5xl mx-auto">
+        <PostBox subreddit={topic as string} styling={{ top: "top-16" }} />
+        <Feed
+          subreddit={topic as string}
+          styling={{ marginTop: "mt-12", marginBottom: "mb-16" }}
+        />
+      </div>
     </div>
-
+  );
 }
